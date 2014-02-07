7876eaf1bcc043eb89232433f92f82aa5c461bd5
Joel Cantoral <jcantoral@tir.com.gt>
2014-02-07 11:22:17 -0600

    new version of dfs algorithm plus minimax implementation on top of it

--- main.js
+++ main.js
@@ -1,53 +1,144 @@
 /** main.js **/
-var graph = {
-    "1":  {},
-    "2":  {},
-    "3":  {},
-    "4":  {},
-    "5":  {},
-    "6":  {},
-    "7":  {},
-    "8":  {},
-    "9":  {},
-    "10": {},
-    "11": {},
-    "12": {}
-};
+function Minimax(){
+    this.dfs = function (node, level) {
+        level = typeof level !== 'undefined' ? level : 1;
+        if (!node.hasChildren()) {
+            return node.getValue();
+        }
+        var i, children = node.getChildren(), child, childValue;
+        for (i = 0; i < children.length; i += 1) {
+            child = children[i];
+            childValue = this.dfs(child, level+1);
+            if (node.getValue() ==='unset' && i === (children.length-1)) {
+                //Si el nodo padre no tiene ningun valor asignado
+                // y es el ultimo hijo; devolver su valor al padre.
+                return node.getValue();
+            }else if (node.getValue() === 'unset' && i !== (children.length-1)){
+                //Si el nodo padre no tiene ningun valor asignado
+                // pero no es el ultimo hijo; asignar su valor al padre y continuar.
+                node.setValue(childValue);
+            }else if (node.getValue() !== 'unset' && i === (children.length-1)){
+                //Si el nodo padre tiene un valor asignado 
+                // y es el ultimo hijo; asignar el mejor valor al padre y devolverlo.
+                if (this.isMin(level)){
+                    node.setValue(Math.min(node.getValue(), childValue));
+                }else{
+                    node.setValue(Math.max(node.getValue(), childValue));
+                }
+                return node.getValue();
+            }else if (node.getValue() !== 'unset' && i !== (children.length-1)){
+                // Si el nodo padre tiene un valor asignado
+                // pero no es el ultimo hijo; asignar el mejor valor al padre y continuar
+                if (this.isMin(level)){
+                    node.setValue(Math.min(node.getValue(), childValue));
+                }else{
+                    node.setValue(Math.max(node.getValue(), childValue));
+                }
+            }
+        }
+    };
 
-var problem2 = {
-    "1":{ "2":"", "3":"", "4":"8" },
-    "2":{ "5":"", "6":"6", "7":"" },
-    "3":{ "8":"0", "9":"", "10":"" },
-    "4":{ "11":"8", "12":"", "13":"" },
-    "5":{ "14":"", "15":"17" },
-    "6":{ "16":"6", "17":"" },
-    "8":{ "18":"0" },
-    "9":{ "19":"10", "20":"" },
-    "10":{ "21":"", "22":"20" },
-    "11":{ "23":"", "24":"8" },
-    "13":{ "25":"", "26":"9", "27":"" },
-    "14":{ "28":"", "29":"9" },
-    "16":{ "30":"6", "31":"" },
-    "17":{ "32":"4" },
-    "18":{ "33":"", "34":"0" },
-    "20":{ "35":"3", "36":"", "37":"" },
-    "23":{ "38":"3", "39":"", "40":"" },
-    "25":{ "41":"7", "42":"" },
-    "27":{ "43":"-1", "44":"" },
-    "31":{ "45":"7", "46":"" },
-    "32":{ "47":"", "48":"", "49":"4" },
-    "36":{ "50":"", "51":"16" },
-    "42":{ "52":"8", "53":"" }
-  }
+    this.isMin = function(level){
+        return level % 2 === 0;
+    };
 
-[[1,2], [1,7], [1,8], [2,3], [2,6], [3,4], [3,5], [8,9], [8,12], [9,10], [9,11]].forEach(function (edge) {
-    graph[edge[0]][edge[1]] = graph[edge[1]];
-    graph[edge[1]][edge[0]] = graph[edge[0]];
-});
+    this.getBestPath = function(treeNodes){
 
-var visited = [];
-dfs(1, graph, function (n) {
-    visited.push(n);
-});
+    };
+}
 
-console.log(visited.join(', '));
+function  Node(givenName, givenValue) {
+    this.children = [];
+    this.value = givenValue;
+    this.name = givenName;
+
+    this.addChildren = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            this.children.push(arguments[i]);
+        }
+    };
+
+    this.hasChildren = function () {
+        return this.children.length > 0;
+    };
+
+    this.getChildren = function () {
+        return this.children;
+    };
+
+    this.getName = function(){
+        return this.name;
+    };
+
+    this.setValue = function(newValue){
+        this.value = newValue;
+    };
+
+    this.getValue = function () {
+        return this.value;
+    };
+}
+
+function GraphVisualizer(){
+    this.graph = {};
+    this.buildGraph = function(node){
+        var nodeDescription = {
+            "name":node.getName() + " ("+ node.getValue() +")",
+            "children":[],
+            "size":1
+        };
+        if (!node.hasChildren()) {
+            nodeDescription["name"] = node.getName() + " ("+ node.getValue() +")";
+            nodeDescription["size"] = node.getValue();
+            return nodeDescription;
+        }
+        var i, children = node.getChildren(), child;
+        for (i = 0; i < children.length; i += 1) {
+            child = children[i];
+            childDescription = this.buildGraph(child);
+            nodeDescription["children"].push(childDescription);
+        }
+        return nodeDescription;
+    };
+    this.generateGraph = function(jsonObject){
+        this.graph = jsonObject;
+        //Escribir al directorio
+        var jsonContents = JSON.stringify(this.graph);
+        var blob = new Blob([jsonContents], {type: "text/plain;charset=utf-8"});
+        var filename = 'jsonTree.json';
+        saveAs(blob, filename);
+
+        this.showGraph('jsonTree.json');
+    };
+    this.showGraph = function (filename){
+        treeLoader = new FileLoader(filename);
+    };
+}
+
+//Leer archivo y cargar configuracion
+var node1 = new Node('node1', 'unset');
+var node2 = new Node('node2', 'unset');
+var node3 = new Node('node3', 'unset');
+var node4 = new Node('node4', 4);
+var node5 = new Node('node5', 5);
+var node6 = new Node('node6', 6);
+var node7 = new Node('node7', 7);
+var node8 = new Node('node8', 'unset');
+var node9 = new Node('node9', 'unset');
+var node10 = new Node('node10', 10);
+var node11 = new Node('node11', 11);
+var node12 = new Node('node12', 12);
+
+node1.addChildren(node2, node7, node8);
+node2.addChildren(node3, node6);
+node3.addChildren(node4, node5);
+node8.addChildren(node9, node12);
+node9.addChildren(node10, node11);
+
+var root = node1;
+
+var minimax = new Minimax();
+var graphVisualizer = new GraphVisualizer();
+var result = minimax.dfs(root);
+var jsonTree = graphVisualizer.buildGraph(root);
+graphVisualizer.generateGraph(jsonTree);
