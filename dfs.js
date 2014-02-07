7a35fc65a3776e88ea1d7df2ef087f68a9fcc009
Joel Cantoral <cs.joel.09@gmail.com>
2014-02-06 23:40:43 -0600

    momentanously stripped the module exports to test without node

--- dfs.js
+++ dfs.js
@@ -1,24 +1,17 @@
-module.exports = (function () {
 
-    'use strict';
+function visit(graph, fn, visited) {
 
-    function visit(graph, fn, visited) {
-
-        for (var node in graph) {
-            if (-1 === visited.indexOf(node)) {
-                visited.push(node);
-                fn(node);
-                visit(graph[node], fn, visited);
-            }
+    for (var node in graph) {
+        if (-1 === visited.indexOf(node)) {
+            visited.push(node);
+            fn(node);
+            visit(graph[node], fn, visited);
         }
-    };
-
-    function dfs(node, graph, fn) {
-        var nodes = {};
-        nodes[node] = graph[node];
-        visit(nodes, fn, []);
     }
+};
 
-    return dfs;
-
-})();
+function dfs(node, graph, fn) {
+    var nodes = {};
+    nodes[node] = graph[node];
+    visit(nodes, fn, []);
+}
